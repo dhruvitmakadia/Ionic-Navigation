dcac88b6026253c005b54a39f37def58ce6d8f4b
amit <amitAC81@gmail.com>
2020-05-08 16:51:50 +0530

    fix: app exit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, IonRouterOutlet, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  // set up hardware back button event.
+  lastTimeBackPress = 0;
+  timePeriodToExit = 2000;
+
+  // code for exit app
+  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private toastController: ToastController,
+    private router: Router
   ) {
+    this.backButtonEvent();
     this.initializeApp();
   }
 
@@ -22,6 +32,33 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+  }
+  // active hardware back button
+  backButtonEvent() {
+    this.platform.backButton.subscribe(async () => {
+
+      this.routerOutlets.forEach(async (outlet: IonRouterOutlet) => {
+        if (outlet && outlet.canGoBack()) {
+          outlet.pop();
+
+        } else if (this.router.url === '/home') {
+          if (new Date().getTime() - this.lastTimeBackPress < this.timePeriodToExit) {
+            // this.platform.exitApp(); // Exit from app
+            navigator['app'].exitApp(); // work in ionic 4
+
+          } else {
+            const toast = await this.toastController.create({
+              message: 'Press back again to exit App.',
+              duration: 2000,
+              position: 'middle'
+            });
+            toast.present();
+            // console.log(JSON.stringify(toast));
+            this.lastTimeBackPress = new Date().getTime();
+          }
+        }
+      });
     });
   }
 }
